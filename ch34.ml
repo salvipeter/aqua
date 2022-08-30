04494e5a1ddd654755c9453a269732b69d9c28d4
Peter Salvi <vukung@yahoo.com>
2022-08-30 12:25:08 +0200

    Challenge 34 - bugfix

--- ch34.ml
+++ ch34.ml
@@ -1,12 +1,10 @@
-#use "ch34-input.ml";;
+(* #use "ch34-input.ml";; *)
 
-(*
 let input = [|[|"station";"r1";"r2";"r3"|];
               [|"a";"00:01";"";"00:02"|];
               [|"b";"00:16";"";"00:17"|];
               [|"c";"";"00:21";""|];
               [|"d";"00:46";"00:51";"00:47"|]|]
-*)
 
 let to_mins str =
     let digit c = int_of_char c - int_of_char '0' in
@@ -17,17 +15,21 @@ let to_mins str =
 (* name, last_station, [(station, time); ...] *)
 type train = Train of string * string * (string * int) list
 
-(* name, Some (train, time_in), [(waiting_train_name, last_station); ...] *)
-type station = Station of string * (string * int) option * (string * string) list
+(* name, Some (train, time_in), [(waiting_train_name, last_station, original_time); ...] *)
+type station = Station of string * (string * int) option * (string * string * int) list
 
 (* Inserts train t into the waiting queue *)
-let rec insert_queue (Train (name, last, _) as t) = function
-    | []         -> [(name, last)]
-    | (n,l)::nls ->
-            if last < l || last = l && name < n then
-                (name, last) :: (n, l) :: nls
-            else
-                (n, l) :: insert_queue t nls
+let rec insert_queue t qs =
+    match t with
+    | Train (_, _, []) -> failwith "invalid train"
+    | Train (name, last, (_, time) :: stops) ->
+            match qs with
+            | [] -> [(name, last, time)]
+            | (n, l, _) as q :: qs ->
+                    if last < l || last = l && name < n then
+                        (name, last, time) :: q :: qs
+                    else
+                        q :: insert_queue t qs
 
 (* Updates the station list s.t. train t enters the queue of station named sname *)
 let rec enter_queue t sname = function
@@ -50,26 +52,26 @@ let arrive time ts ss =
                         if time' <> time then
                             f ss (t :: acc) ts
                         else begin
-                            (* print_string "Train "; print_string name;
-                            print_string " enters the queue at "; print_endline s; *)
+                            print_string "Train "; print_string name;
+                            print_string " enters the queue at "; print_endline s;
                             let ss' = enter_queue t s ss in
                             f ss' (Train (name, s, ns) :: acc) ts
                         end
     in f ss [] ts
 
-(* Adds 5 minutes to all times in the train's schedule *)
-let rec wait5 tname = function
+(* Adds n minutes to all times in the train's schedule *)
+let rec wait n tname = function
     | [] -> failwith "no such train"
     | Train (name, last, stops) as t :: ts ->
             if name <> tname then
-                t :: wait5 tname ts
+                t :: wait n tname ts
             else
-                let wait (stop, time) = stop, time + 5 in
-                let stops' = List.map wait stops in
+                let add (stop, time) = stop, time + n in
+                let stops' = List.map add stops in
                 Train (name, last, stops') :: ts
 
 (* Next trains enter all empty stations *)
-let enter_station ts ss =
+let enter_station time ts ss =
     let rec f ts acc = function
         | [] ->
                 ts, acc
@@ -77,11 +79,11 @@ let enter_station ts ss =
                 f ts (s :: acc) ss
         | Station (_, None, []) as s :: ss ->
                 f ts (s :: acc) ss
-        | Station (name, None, (t, _)::queue) :: ss ->
-                (* print_string "Train "; print_string t;
-                print_string " enters station "; print_endline name; *)
+        | Station (name, None, (t, _, time')::queue) :: ss ->
+                print_string "Train "; print_string t;
+                print_string " enters station "; print_endline name;
                 let s' = Station (name, Some (t, 1), queue) in
-                let ts' = wait5 t ts in
+                let ts' = wait (time - time' + 5) t ts in
                 f ts' (s' :: acc) ss
     in f ts [] ss
 
@@ -101,18 +103,18 @@ let depart ts ss =
     | [] -> acc, finished
     | Station (_, None, _) as s :: ss ->
             f finished (s :: acc) ss
-    | Station (name, Some (t, n), queue) as s :: ss ->
+    | Station (name, Some (t, n), queue) :: ss ->
             if n < 5 then
                 let s' = Station (name, Some (t, n + 1), queue) in
                 f finished (s' :: acc) ss
             else begin
-                (* print_string "Train "; print_string t;
-                print_string " departs station "; print_endline name; *)
+                print_string "Train "; print_string t;
+                print_string " departs station "; print_endline name;
                 let s' = Station (name, None, queue) in
                 if completed_journey t ts then
                     begin
-                        (* print_string "Train "; print_string t;
-                        print_endline " completes its journey"; *)
+                        print_string "Train "; print_string t;
+                        print_endline " completes its journey";
                         f (t :: finished) (s' :: acc) ss
                     end
                 else
@@ -143,26 +145,33 @@ let start_time table tname =
             in find 1
     in loop 1
 
-let answer =
-    let n = Array.length input - 1 in
+let init_trains table =
     let m = Array.length input.(0) - 1 in
+    let n = Array.length input - 1 in
     let rec stops t s =
         if s > n then []
         else match input.(s).(t) with
         | ""   -> stops t (s + 1)
         | time -> (input.(s).(0), to_mins time) :: stops t (s + 1) in
     let train t = Train (input.(0).(t), "", stops t 1) in
-    let ts = Seq.ints 1 |> Seq.take m |> Seq.map train |> List.of_seq in
+    Seq.ints 1 |> Seq.take m |> Seq.map train |> List.of_seq
+
+let init_stations table =
+    let n = Array.length input - 1 in
     let station s = Station (input.(s).(0), None, []) in
-    let ss = Seq.ints 1 |> Seq.take n |> Seq.map station |> List.of_seq in
+    Seq.ints 1 |> Seq.take n |> Seq.map station |> List.of_seq
+
+let answer =
+    let ts = init_trains input in
+    let ss = init_stations input in
     let max_time = ref 0 in
     let rec process ts ss time =
-        (* print_string "["; print_int time; print_endline "]"; *)
+        print_string "["; print_int time; print_endline "]";
         let ts, ss = arrive time ts ss in
         let ss, finishing = depart ts ss in
         let finish_times = List.map (fun t -> time - start_time input t) finishing in
         max_time := List.fold_left max !max_time finish_times;
-        let ts, ss = enter_station ts ss in
+        let ts, ss = enter_station time ts ss in
         if not (finished ts ss) then
             process ts ss (time + 1)
     in process ts ss 0;
